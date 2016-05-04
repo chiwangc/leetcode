e0a0c307cbc66c90e99a796b653f4a8ec30fd37e
Chiwang Chow <chow@chiwang.me>
2016-05-04 23:32:00 +0800

    Solution for problem 27

---
+++ src/me/chiwang/leetcode/problem027/Solution.java
@@ -0,0 +1,15 @@
+package me.chiwang.leetcode.problem027;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+    	int count = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] != val) {
+    			nums[count++] = nums[i];
+    		}
+    	}
+    	
+        return count;
+    }
+}
