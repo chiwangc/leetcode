227b5fcc5a5315f124cf5c3cd3b86a96e2a6e0bb
Chiwang Chow <chow@chiwang.me>
2016-05-06 23:13:34 +0800

    Solution for problem 29

---
+++ src/me/chiwang/leetcode/problem029/Solution.java
@@ -0,0 +1,53 @@
+package me.chiwang.leetcode.problem029;
+
+public class Solution {
+    public int divide(int dividend, int divisor) {
+	// Exception handling
+	if (divisor == 0) {
+	    return Integer.MAX_VALUE;
+	}
+	
+	if (dividend == Integer.MIN_VALUE) {
+	    // TODO handle
+	}
+	
+	boolean isNeg = (dividend < 0) ^ (divisor < 0);
+	int negAbsDividend = dividend < 0 ? dividend : -dividend;
+	int negAbsDivisor = divisor < 0 ? divisor : -divisor;
+	
+	int[] nums = new int[64];
+	int logn = -1;
+	
+	while (negAbsDividend <= negAbsDivisor) {
+	    nums[++logn] = negAbsDivisor;
+	    negAbsDivisor = negAbsDivisor + negAbsDivisor;
+	    if (negAbsDivisor >= 0) {
+		// Underflow
+		break;
+	    }
+	}
+	
+	for (int i = logn; i >= 0; i--) {
+	    if (negAbsDividend <= nums[i]) {
+		negAbsDividend = negAbsDividend - nums[i];
+		nums[i] = 1;
+	    } else {
+		nums[i] = 0;
+	    }
+	}
+	
+	int sum = 0;
+	
+	for (int i = 0; i <= logn; i++) {
+	    sum = sum - nums[i] * (1 << i);
+	}
+	
+	if (sum == Integer.MIN_VALUE && !isNeg) {
+	    // Overflow
+	    return Integer.MAX_VALUE;
+	}
+
+	return isNeg ? sum : -sum;
+
+    }
+}
